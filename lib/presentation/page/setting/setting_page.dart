3a788d8ce7d8de0932cb8dcf12fb189968d84c60
h.kuwabara <h.kuwabara1103@gmail.com>
2022-11-16 20:19:47 +0900

    🚀 feat: 戻るアイコンは自動的に設置されるため、アイコン設定を削除

--- lib/presentation/page/setting/setting_page.dart
+++ lib/presentation/page/setting/setting_page.dart
@@ -9,10 +9,6 @@ class SettingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
         title: const Text('設定'),
       ),
       body: Column(
