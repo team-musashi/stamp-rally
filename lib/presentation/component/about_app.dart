e795ef443a567f7e7350e5f5af58571eabc82daf
h.kuwabara <h.kuwabara1103@gmail.com>
2022-11-16 22:15:22 +0900

    ✨ style: 不要なコメントを削除

--- lib/presentation/component/about_app.dart
+++ lib/presentation/component/about_app.dart
@@ -19,7 +19,6 @@ class AboutAppSection extends ConsumerWidget {
             const Icon(Icons.apple_sharp), // Todo アプリIconが決まったら差し替え
         applicationLegalese: '(C)2022 stamp-rally', // Todo 権利情報が決まったら差し替え
       ),
-      // Todo PackageInfoPlusから取得する
       child: const AboutApp(
         applicationVersion: applicationVersion,
       ),
