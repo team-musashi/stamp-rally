e0bf98929da6a2e79efe348f251fd217d4537033
h.kuwabara <h.kuwabara1103@gmail.com>
2022-11-16 21:12:54 +0900

    🚀 feat: このアプリについてセクションのUIを実装

--- lib/presentation/page/setting/setting_page.dart
+++ lib/presentation/page/setting/setting_page.dart
@@ -12,8 +12,31 @@ class SettingPage extends StatelessWidget {
         title: const Text('設定'),
       ),
       body: Column(
-        children: const [
-          DeleteUserButton(),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const DeleteUserButton(),
+          GestureDetector(
+            onTap: () => showAboutDialog(
+                context: context,
+                applicationName: 'Stamp-Rally',
+                applicationVersion: '1.0.0',
+                applicationIcon: const Icon(Icons.apple_sharp),
+                applicationLegalese: '(C)2022 stamp-rally'),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('このアプリについて', style: TextStyle(fontSize: 16)),
+                  Text(
+                    'v1.0.0',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
