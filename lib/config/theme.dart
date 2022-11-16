0ea05b76cf18a712fa72ac484095c64767f527bf
susatthi <susa@keyber.jp>
2022-11-17 06:46:43 +0900

    🚀 feat:  不要なテーマ指定を削除
    まだデザインは適当でよいので現時点では不要 #22

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -4,10 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 /// テーマプロバイダー
 final themeProvider = Provider(
   (ref) => ThemeData(
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.blue,
-      foregroundColor: Colors.white,
-    ),
     primarySwatch: Colors.blue,
     snackBarTheme: const SnackBarThemeData(
       behavior: SnackBarBehavior.floating,
