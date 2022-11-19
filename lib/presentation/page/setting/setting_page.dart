70e7ccf19f3a6a975f75d8415ff21af59614573a
h.kuwabara <h.kuwabara1103@gmail.com>
2022-11-20 00:35:18 +0900

    🚀 feat: 設定画面とパッケージ情報プロバイダーを繋ぎこみ

--- lib/presentation/page/setting/setting_page.dart
+++ lib/presentation/page/setting/setting_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../application/package_info/package_info_service.dart';
+import '../../component/async_value_handler.dart';
 import '../../component/delete_user.dart';
 
 /// 設定画面
@@ -36,28 +39,27 @@ class _Body extends StatelessWidget {
 }
 
 /// 「このアプリについて」のListTile
-class _AboutAppListTile extends StatelessWidget {
+class _AboutAppListTile extends ConsumerWidget {
   const _AboutAppListTile();
 
   @override
-  Widget build(BuildContext context) {
-    // Todo PackageInfoPlusのProviderから取得する
-    const applicationName = 'Stamp-Rally';
-    const applicationVersion = '1.0.0';
-
-    return ListTile(
-      title: const Text('このアプリについて'),
-      subtitle: const Text('v$applicationVersion'),
-      onTap: () {
-        showAboutDialog(
-          context: context,
-          applicationName: applicationName,
-          applicationVersion: applicationVersion,
-          applicationIcon:
-              const Icon(Icons.apple_sharp), // Todo アプリIconが決まったら差し替え
-          applicationLegalese: '(C)2022 stamp-rally', // Todo 権利情報が決まったら差し替え
-        );
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AsyncValueHandler(
+      value: ref.watch(packageInfoProvider),
+      builder: (info) => ListTile(
+        title: const Text('このアプリについて'),
+        subtitle: Text('v${info.version}'),
+        onTap: () {
+          showAboutDialog(
+            context: context,
+            applicationName: info.appName,
+            applicationVersion: info.version,
+            applicationIcon:
+                const Icon(Icons.apple_sharp), // Todo アプリIconが決まったら差し替え
+            applicationLegalese: '(C)2022 stamp-rally', // Todo 権利情報が決まったら差し替え
+          );
+        },
+      ),
     );
   }
 }
