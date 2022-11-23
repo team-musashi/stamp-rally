549534900cd08ffdf02b1ce04bd3904f18db5e23
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-11-23 12:54:08 +0900

    🚀 feat: スタンプラリー詳細画面を仮のデザインで作成した
    #50

--- lib/presentation/page/stamp_rally/stamp_rally_explanation_page.dart
+++ lib/presentation/page/stamp_rally/stamp_rally_explanation_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../domain/repository/stamp_rally/entity/stamp_rally.dart';
+import '../../component/delimiter_block.dart';
 
 /// スタンプラリー詳細画面
 class StampRallyExplanationPage extends ConsumerWidget {
@@ -12,7 +13,23 @@ class StampRallyExplanationPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text('スタンプラリー詳細')),
-      body: Text(stampRally.title),
+      body: Column(
+        children: [
+          Image(image: NetworkImage(stampRally.imageUrl)),
+          Text(
+            stampRally.title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          //todo
+          const Text('チェックポイント数'),
+          Text('所要時間：${stampRally.requiredTime.toString()}時間'),
+          DelimiterBlock(text: stampRally.explanation),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('参加する'),
+          ),
+        ],
+      ),
     );
   }
 }
