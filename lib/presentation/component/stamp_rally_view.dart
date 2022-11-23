572b201734ea182d417cbbeeea8d137f7ff6c764
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-11-23 11:46:30 +0900

    🚀 feat: StampRallyViewItemタップ時にスタンプラリー詳細画面へ遷移するようにした
    #50

--- lib/presentation/component/stamp_rally_view.dart
+++ lib/presentation/component/stamp_rally_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../domain/repository/stamp_rally/entity/stamp_rally.dart';
 import '../../domain/repository/stamp_rally/stamp_rally_repository.dart';
+import '../router.dart';
 import 'async_value_handler.dart';
 
 /// スタンプラリー概要を表示するためのウィジェット
@@ -12,40 +13,43 @@ class StampRallyViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 12, left: 6, right: 6),
-      decoration: const BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey, //色
-            spreadRadius: 1,
-            blurRadius: 5,
-            offset: Offset(1, 1),
-          ),
-        ],
-        color: Colors.white,
-      ),
-      child: Row(
-        children: [
-          Image(
-            image: NetworkImage(item.imageUrl),
-            width: 200,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  item.title,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text('開催場所： ${item.place}'),
-                Text('所要時間： ${item.requiredTime}時間'),
-              ],
+    return InkWell(
+      onTap: () => const StampRallyExplanationRoute().push(context),
+      child: Container(
+        margin: const EdgeInsets.only(top: 12, left: 6, right: 6),
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey, //色
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: Offset(1, 1),
             ),
-          )
-        ],
+          ],
+          color: Colors.white,
+        ),
+        child: Row(
+          children: [
+            Image(
+              image: NetworkImage(item.imageUrl),
+              width: 200,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.title,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text('開催場所： ${item.place}'),
+                  Text('所要時間： ${item.requiredTime}時間'),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/presentation/page/stamp_rally/stamp_rally_explanation_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// スタンプラリー詳細画面
+class StampRallyExplanationPage extends StatelessWidget {
+  const StampRallyExplanationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('スタンプラリー詳細')),
+    );
+  }
+}
