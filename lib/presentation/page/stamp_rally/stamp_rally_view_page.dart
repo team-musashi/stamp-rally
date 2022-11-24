6ab0343942861a65ed9ab1cd756a3e293b332328
susatthi <susa@keyber.jp>
2022-11-24 10:32:46 +0900

    ♻️ refactor:  AppBarを再描画しないように再描画領域を狭くした
    #50

--- lib/presentation/page/stamp_rally/stamp_rally_view_page.dart
+++ lib/presentation/page/stamp_rally/stamp_rally_view_page.dart
@@ -6,33 +6,47 @@ import '../../component/async_value_handler.dart';
 import '../../component/delimiter_block.dart';
 
 /// スタンプラリー詳細画面
-class StampRallyViewPage extends ConsumerWidget {
+class StampRallyViewPage extends StatelessWidget {
   const StampRallyViewPage({super.key});
 
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('スタンプラリー詳細'),
+      ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends ConsumerWidget {
+  const _Body();
+
+  @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // アプリケーション層でStampRally.spotsにデータを詰めてもらう
     return AsyncValueHandler(
       value: ref.watch(currentPublicStampRallyProvider),
-      builder: (stampRally) => Scaffold(
-        appBar: AppBar(title: const Text('スタンプラリー詳細')),
-        body: Column(
-          children: [
-            Image(image: NetworkImage(stampRally.imageUrl)),
-            Text(
-              stampRally.title,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text('チェックポイント数：${stampRally.spots.length}'),
-            Text('所要時間：${stampRally.requiredTime.toString()}時間'),
-            DelimiterBlock(text: stampRally.explanation),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('参加する'),
-            ),
-          ],
-        ),
-      ),
+      builder: (stampRally) {
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              Image(image: NetworkImage(stampRally.imageUrl)),
+              Text(
+                stampRally.title,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text('チェックポイント数：${stampRally.spots.length}'),
+              Text('所要時間：${stampRally.requiredTime.toString()}時間'),
+              DelimiterBlock(text: stampRally.explanation),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('参加する'),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
