5c79eafb879240da4a3a41e09f064b6d4c3e7170
h.kuwabara <h.kuwabara1103@gmail.com>
2022-11-27 16:24:17 +0900

    🚀 feat: プラポリと規約のテキストデザインを変更

--- lib/presentation/component/agreement.dart
+++ lib/presentation/component/agreement.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../application/url_launcher/url_launcher_service.dart';
 import '../../domain/entity/app_info.dart';
-import 'anchor_text.dart';
 
 /// 利用規約とプライバシーポリシーをまとめたウィジェット
 class Agreement extends ConsumerWidget {
@@ -13,23 +12,17 @@ class Agreement extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return RichText(
+      textAlign: TextAlign.center,
       text: TextSpan(
         style: Theme.of(context).textTheme.bodyMedium,
         children: [
           TextSpan(
-            text: 'プライバシーポリシー',
-            style: AnchorText.anchorStyleText(context),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () async {
-                await ref.read(urlLauncherServiceProvider).launch(
-                      ref.read(appInfoProvider).privacyPolicyUrl.toString(),
-                    );
-              },
-          ),
-          const TextSpan(text: ' と '),
-          TextSpan(
-            text: 'サービス利用規約',
-            style: AnchorText.anchorStyleText(context),
+            text: '利用規約',
+            style: const TextStyle(
+              color: Colors.white,
+              decoration: TextDecoration.underline,
+              fontWeight: FontWeight.bold,
+            ),
             recognizer: TapGestureRecognizer()
               ..onTap = () async {
                 await ref.read(urlLauncherServiceProvider).launch(
@@ -37,7 +30,31 @@ class Agreement extends ConsumerWidget {
                     );
               },
           ),
-          const TextSpan(text: ' に同意して始める'),
+          const TextSpan(
+            text: ' と ',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          TextSpan(
+            text: 'プライバシーポリシー',
+            style: const TextStyle(
+              color: Colors.white,
+              decoration: TextDecoration.underline,
+              fontWeight: FontWeight.bold,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () async {
+                await ref.read(urlLauncherServiceProvider).launch(
+                      ref.read(appInfoProvider).privacyPolicyUrl.toString(),
+                    );
+              },
+          ),
+          const TextSpan(
+            text: ' に\n同意の上ご利用ください',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ],
       ),
     );
