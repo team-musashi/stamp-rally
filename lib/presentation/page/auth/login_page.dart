b52dc02d8c34b937211b486a69476c20143912ba
susatthi <susa@keyber.jp>
2022-11-28 06:42:51 +0900

    ♻️ refactor:  Gapを使うように修正
    #58

--- lib/presentation/page/auth/login_page.dart
+++ lib/presentation/page/auth/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 import '../../../util/assets/assets.gen.dart';
 import '../../component/agreement.dart';
@@ -41,9 +42,7 @@ class LoginPage extends StatelessWidget {
                     child: CreateUserButton(),
                   ),
                 ),
-                SizedBox(
-                  height: 12,
-                ),
+                Gap(12),
                 Agreement()
               ],
             )
