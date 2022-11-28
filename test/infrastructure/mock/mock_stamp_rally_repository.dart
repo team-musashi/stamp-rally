bb85a0dd5eace8ffbd69d4daad0257c3c78d20a9
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-11-29 03:49:41 +0900

    🐛 fix: 不要メソッドの削除
    #51

--- test/infrastructure/mock/mock_stamp_rally_repository.dart
+++ test/infrastructure/mock/mock_stamp_rally_repository.dart
@@ -53,10 +53,4 @@ class MockStampRallyRepository implements StampRallyRepository {
     // TODO(cobo): implement fetchSpots
     throw UnimplementedError();
   }
-
-  @override
-  Future<void> entryStampRally({required String stampRallyId}) {
-    // TODO(cobo): implement entryStampRally
-    throw UnimplementedError();
-  }
 }
