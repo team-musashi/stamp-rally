067d634bc6d6ab70f60b42df7739f1f105b1955a
h.kuwabara <h.kuwabara1103@gmail.com>
2022-12-06 20:44:07 +0900

    🚀 feat: ホーム画面にタブを追加

--- lib/presentation/page/home/home_page.dart
+++ lib/presentation/page/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../../util/assets/assets.gen.dart';
-import '../../component/delimiter_block.dart';
 import '../../component/stamp_rally_view.dart';
 import '../../router.dart';
 
@@ -11,31 +10,63 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          children: [
-            // Todo: アイコンが確定次第差し替える
-            Assets.images.iconStamp.image(
-                height: 12, color: Theme.of(context).colorScheme.primary),
-            Assets.images.logoStampRarry.image(
-                height: 32, color: Theme.of(context).colorScheme.primary),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Column(
+            children: [
+              // Todo: アイコンが確定次第差し替える
+              Assets.images.iconStamp.image(
+                  height: 12, color: Theme.of(context).colorScheme.primary),
+              Assets.images.logoStampRarry.image(
+                  height: 32, color: Theme.of(context).colorScheme.primary),
+            ],
+          ),
+          bottom: TabBar(
+            tabs: const [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: Text(
+                  '公開中',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: Text(
+                  '参加中',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: Text(
+                  '完了済',
+                ),
+              )
+            ],
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(30), // Creates border
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            indicatorPadding: const EdgeInsets.symmetric(horizontal: 8),
+            labelColor: Theme.of(context).colorScheme.background,
+            unselectedLabelColor: Colors.black,
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () => const SettingRoute().push(context),
+              icon: const Icon(Icons.settings),
+            )
           ],
         ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () => const SettingRoute().push(context),
-            icon: const Icon(Icons.settings),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: const [
-            DelimiterBlock(text: '参加中のスタンプラリー'),
-            DelimiterBlock(text: '開催中のスタンプラリー'),
-            StampRallyListView(),
+        body: const TabBarView(
+          children: [
+            SingleChildScrollView(
+              child: StampRallyListView(),
+            ),
+            Center(child: Text('参加中')), // Todo: 参加中ListView
+            Center(child: Text('完了済')) // Todo: 完了済ListView
           ],
         ),
       ),
