8b67559beb4bd5de44108ceb18565957e46c6a29
h.kuwabara <h.kuwabara1103@gmail.com>
2022-12-11 14:09:22 +0900

    🚀 feat: 参加中画面のワイヤーフレームを仮作成

--- lib/presentation/page/home/component/entry_view.dart
+++ lib/presentation/page/home/component/entry_view.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../../domain/repository/stamp_rally/entity/stamp_rally.dart';
+import '../../../../domain/repository/stamp_rally/stamp_rally_repository.dart';
+import '../../../component/async_value_handler.dart';
 
 /// 参加中画面
-class EntryView extends StatelessWidget {
+class EntryView extends ConsumerWidget {
   const EntryView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO(some): 参加中ListView
-    return const Center(child: Text('参加中'));
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AsyncValueHandler<List<StampRally>>(
+      value: ref.watch(entryStampRalliesProvider),
+      builder: (stampRallies) {
+        if (stampRallies.isEmpty) {
+          return const Text('参加中のスタンプラリーはありません');
+        } else {
+          final stampRally = stampRallies[0];
+          return Text(stampRally.title); // ToDo Figmaにあわせてデザイン実装
+        }
+      },
+    );
   }
 }
