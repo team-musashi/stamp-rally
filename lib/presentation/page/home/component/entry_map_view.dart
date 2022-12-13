064963e235e53464de82cdf89b4421f310d6ed98
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-12-14 03:57:21 +0900

    🚀 feat: GoogleMap表示の下準備完了
    #74

--- lib/presentation/page/home/component/entry_map_view.dart
+++ lib/presentation/page/home/component/entry_map_view.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class EntryMapView extends StatelessWidget {
+/// 参加中スタンプラリーのスポットマップ表示用View
+class EntryMapView extends StatefulWidget {
   const EntryMapView({super.key});
 
   @override
+  State<EntryMapView> createState() => _EntryMapViewState();
+}
+
+class _EntryMapViewState extends State<EntryMapView> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text(''),
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 11,
+      ),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
 }
