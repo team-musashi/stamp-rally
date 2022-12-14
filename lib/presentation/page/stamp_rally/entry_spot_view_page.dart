9eee7736d1e23cd498b31bbaf4ac48977552736c
yuto <hryt.totooria@gmail.com>
2022-12-15 01:00:45 +0900

    🚀 feat: image_picker の取得結果を画面に表示する
    Closes #75

--- lib/presentation/page/stamp_rally/entry_spot_view_page.dart
+++ lib/presentation/page/stamp_rally/entry_spot_view_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../application/image_picker/image_picker_service.dart';
+import '../../../application/image_picker/state/image_picker_result.dart';
 import '../../../application/stamp_rally/state/current_entry_spot.dart';
 import '../../component/async_value_handler.dart';
 import '../../component/delimiter_block.dart';
@@ -25,6 +27,7 @@ class _Body extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final pickedImage = ref.watch(imagePickerResultProvider);
     return AsyncValueHandler(
       value: ref.watch(currentEntrySpotProvider),
       builder: (spot) {
@@ -32,9 +35,27 @@ class _Body extends ConsumerWidget {
         final tel = spot.tel;
         return Column(
           children: [
-            Image(
-              image: NetworkImage(spot.imageUrl),
-            ),
+            // 撮影された画像が存在する場合、表示を切り替える
+            pickedImage != null
+                ? Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      SizedBox(
+                        height: 300,
+                        child: Image.file(
+                          pickedImage,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('この画像をアップロードする'),
+                      ),
+                    ],
+                  )
+                // 撮影された画像が存在しない場合、スポットの写真を表示する
+                : Image(
+                    image: NetworkImage(spot.imageUrl),
+                  ),
             Text(spot.title),
             if (address != null) Text(address),
             if (tel != null) Text(tel),
@@ -43,11 +64,19 @@ class _Body extends ConsumerWidget {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    await ref
+                        .read(imagePickerServiceProvider)
+                        .pickImageByCamera();
+                  },
                   child: const Text('カメラを開く'),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    await ref
+                        .read(imagePickerServiceProvider)
+                        .pickImageByGallery();
+                  },
                   child: const Text('ギャラリーを開く'),
                 ),
               ],
