22577ef3b6a5baaab13fbd1a88a53f6833364502
yuto <hryt.totooria@gmail.com>
2022-12-15 02:18:00 +0900

    🚀 feat: Firebase Storage インスタンスを生成
    Closes #75

--- lib/infrastructure/firebase/firebase.dart
+++ lib/infrastructure/firebase/firebase.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// FirebaseAuthプロバイダー
@@ -20,6 +21,11 @@ final firebaseUserIdProvider = Provider(
 /// FirebaseFirestoreプロバイダー
 final firebaseFirestoreProvider = Provider(
   (ref) => FirebaseFirestore.instance,
+);
+
+/// FirebaseStorageプロバイダー
+final firebaseStorageProvider = Provider(
+  (ref) => FirebaseStorage.instance,
 );
 
 /// ユーザーコレクション参照プロバイダー
