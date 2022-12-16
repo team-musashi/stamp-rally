7f524800484de81761c289674019720f003b5d62
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-12-16 19:18:44 +0900

    🚀 feat: 参加中スタンプラリーのスポットをマップ上にピンとして立てられるようにした
    #74

--- lib/presentation/page/home/component/entry_map_view.dart
+++ lib/presentation/page/home/component/entry_map_view.dart
@@ -1,34 +1,57 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../../../../application/stamp_rally/state/current_entry_stamp_rally.dart';
+import '../../../../domain/repository/stamp_rally/entity/spot.dart';
+import '../../../component/async_value_handler.dart';
+
 /// 参加中スタンプラリーのスポットマップ表示用View
-class EntryMapView extends StatefulWidget {
+class EntryMapView extends ConsumerWidget {
   const EntryMapView({super.key});
 
-  @override
-  State<EntryMapView> createState() => _EntryMapViewState();
-}
+//   @override
+//   _EntryMapViewState createState() => _EntryMapViewState();
+// }
 
-class _EntryMapViewState extends State<EntryMapView> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+// class _EntryMapViewState extends ConsumerState<EntryMapView> {
+  static final Completer<GoogleMapController> _controller = Completer();
 
   @override
-  Widget build(BuildContext context) {
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 11,
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AsyncValueHandler(
+      value: ref.watch(currentEntryStampRallyProvider),
+      builder: (stampRally) {
+        return Scaffold(
+          body: GoogleMap(
+            onMapCreated: _controller.complete,
+            markers: createMarkersFromSpots(stampRally.spots),
+            initialCameraPosition: CameraPosition(
+              target: LatLng(
+                stampRally.spots.first.location.latitude,
+                stampRally.spots.first.location.longitude,
+              ),
+              zoom: 14,
+            ),
+          ),
+        );
+      },
     );
   }
 
-  void _onMapCreated(GoogleMapController? controller) {
-    if (controller == null) {
-      return;
+  /// 参加中スタンプラリーのスポットからマップに表示するマーカーを生成する
+  Set<Marker> createMarkersFromSpots(List<Spot> spots) {
+    final markers = <Marker>{};
+    for (final spot in spots) {
+      markers.add(
+        Marker(
+          markerId: MarkerId(spot.id),
+          position: LatLng(spot.location.latitude, spot.location.longitude),
+        ),
+      );
     }
-    mapController = controller;
+    return markers;
   }
 }
