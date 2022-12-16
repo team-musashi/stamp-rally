d31b73960cbd886597bab9de50893039f96c6745
h.kuwabara <h.kuwabara1103@gmail.com>
2022-12-17 00:17:25 +0900

    Revert "♻️ refactor: 不要になった画像を削除"
    This reverts commit 65e70a9a375e8583438a98c6ac8bd89e54bbf59b.

--- lib/util/assets/assets.gen.dart
+++ lib/util/assets/assets.gen.dart
@@ -20,6 +20,10 @@ class $AssetsImagesGen {
   AssetGenImage get iconProd =>
       const AssetGenImage('assets/images/icon_prod.png');
 
+  /// File path: assets/images/icon_stamp.png
+  AssetGenImage get iconStamp =>
+      const AssetGenImage('assets/images/icon_stamp.png');
+
   /// File path: assets/images/logo_burarry.png
   AssetGenImage get logoBurarry =>
       const AssetGenImage('assets/images/logo_burarry.png');
@@ -28,9 +32,19 @@ class $AssetsImagesGen {
   AssetGenImage get logoHeaderBurarry =>
       const AssetGenImage('assets/images/logo_header_burarry.png');
 
+  /// File path: assets/images/logo_stamp_rarry.png
+  AssetGenImage get logoStampRarry =>
+      const AssetGenImage('assets/images/logo_stamp_rarry.png');
+
   /// List of all assets
-  List<AssetGenImage> get values =>
-      [iconDev, iconProd, logoBurarry, logoHeaderBurarry];
+  List<AssetGenImage> get values => [
+        iconDev,
+        iconProd,
+        iconStamp,
+        logoBurarry,
+        logoHeaderBurarry,
+        logoStampRarry
+      ];
 }
 
 class Assets {
