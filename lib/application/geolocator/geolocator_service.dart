520e8a3953cc73ce72523f8aa7a3f9c72c40b7a7
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-12-17 02:59:53 +0900

    ♻️ refactor: 距離計算メソッドを位置情報サービスへ移行
    #74

--- lib/application/geolocator/geolocator_service.dart
+++ lib/application/geolocator/geolocator_service.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -53,5 +55,26 @@ class GeolocatorService {
     // デバイスの位置情報を返す。
     final currentLocation = await Geolocator.getCurrentPosition();
     currentPositionProvider.overrideWithValue(currentLocation);
+  }
+
+  /// 地図上の2つの位置(緯度経度)から距離(km)を求める
+  /// 少数３位を四捨五入して少数第２位まで表示
+  double distanceBetween({
+    required double latitude1,
+    required double longitude1,
+    required double latitude2,
+    required double longitude2,
+  }) {
+    double toRadians(double degree) => degree * math.pi / 180;
+    const r = 6378137; // 地球の半径
+    final f1 = toRadians(latitude1);
+    final f2 = toRadians(latitude2);
+    final l1 = toRadians(longitude1);
+    final l2 = toRadians(longitude2);
+    final a = math.pow(math.sin((f2 - f1) / 2), 2);
+    final b =
+        math.cos(f1) * math.cos(f2) * math.pow(math.sin((l2 - l1) / 2), 2);
+    final d = 2 * r * math.asin(math.sqrt(a + b));
+    return double.parse(d.toStringAsFixed(2));
   }
 }
