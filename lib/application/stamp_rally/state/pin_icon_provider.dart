5bdc84325a3b3e0987a947b32f32220ce281a73f
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-12-19 02:34:28 +0900

    🐛 fix: AndroidとiOSでピンの表示方法が異なるためそれぞれに対応するピンアイコンを用意
    #74

--- lib/application/stamp_rally/state/pin_icon_provider.dart
+++ lib/application/stamp_rally/state/pin_icon_provider.dart
@@ -1,11 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-/// ピンアイコンプロバイダ
-final pinIconProvider = FutureProvider<BitmapDescriptor>(
-  (_) => BitmapDescriptor.fromAssetImage(
-    const ImageConfiguration(devicePixelRatio: 1),
-    'assets/images/marker.png',
-  ),
-);
+/// ピンアイコンプロバイダー
+final pinIconProvider = FutureProvider<BitmapDescriptor>((_) {
+  return BitmapDescriptor.fromAssetImage(
+    const ImageConfiguration(devicePixelRatio: 1, size: Size(10, 10)),
+    Platform.isIOS
+        ? 'assets/images/marker_ios.png'
+        : 'assets/images/marker_android.png',
+  );
+});
