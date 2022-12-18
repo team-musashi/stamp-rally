9180bef157fdd5622b8ef15a24be443dfcc7f9e7
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-12-18 18:08:09 +0900

    🚀 feat: 参加中タブに詳細表示/マップ表示切り替えのトグルボタンを設置
    #74

--- lib/presentation/page/home/component/entry_view.dart
+++ lib/presentation/page/home/component/entry_view.dart
@@ -1,7 +1,8 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
-import 'package:toggle_switch/toggle_switch.dart';
 
-import 'entry_toggle_switch.dart';
+import 'entry_details_view.dart';
+import 'entry_map_view.dart';
 
 /// 参加中画面
 class EntryView extends StatelessWidget {
@@ -9,40 +10,50 @@ class EntryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final view = EntryToggleSwitch.map.view;
-    return Stack(
-      children: [
-        view,
-        Padding(
-          padding: const EdgeInsets.only(top: 15, right: 15),
-          child: Row(
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 1,
+      child: Stack(
+        children: [
+          const Expanded(
+            child: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                EntryDetailsView(),
+                EntryMapView(),
+              ],
+            ),
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ToggleSwitch(
-                minWidth: 60,
-                cornerRadius: 10,
-                activeFgColor: Theme.of(context).colorScheme.primaryContainer,
-                inactiveBgColor: Colors.grey,
-                inactiveFgColor: Colors.white,
-                activeBgColor: const [Colors.white],
-                totalSwitches: 2,
-                icons: const [Icons.list, Icons.map_outlined],
-                iconSize: 20,
-                animate: true,
-                curve: Curves.linearToEaseOut,
-                onToggle: (index) {
-                  // if (index != null) {
-                  //   setState(() {
-                  //     view = EntryToggleSwitch.values[index].view;
-                  //   });
-                  // }
-                },
+              Padding(
+                padding: const EdgeInsets.only(top: 15, right: 15),
+                child: ButtonsTabBar(
+                  backgroundColor: Colors.white,
+                  // ignore: use_named_constants
+                  buttonMargin: const EdgeInsets.symmetric(),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                  borderColor: Colors.white,
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  unselectedLabelStyle: const TextStyle(
+                    color: Colors.white,
+                  ),
+                  unselectedBackgroundColor: Colors.grey,
+                  unselectedBorderColor: Colors.white,
+                  tabs: const [
+                    Tab(icon: Icon(Icons.list)),
+                    Tab(icon: Icon(Icons.map_outlined)),
+                  ],
+                ),
               ),
             ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
