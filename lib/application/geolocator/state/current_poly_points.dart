f1e46191bd41e80d01f6eb76804e7a31e4a50727
susa.keyber <susa@keyber.jp>
2022-12-21 12:39:37 +0900

    ♻️ refactor:  経路算出ロジックをStateに移動
    #74

---
+++ lib/application/geolocator/state/current_poly_points.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../../../domain/entity/app_info.dart';
+import '../../../domain/entity/value_object/geo_location.dart';
+import 'current_geolocator_position.dart';
+
+/// 座標リストとユーザーの現在位置から算出した経路（座標リスト）
+final currentPolylinePointsProvider =
+    FutureProvider.family<List<LatLng>, List<GeoLocation>>(
+  (ref, locations) async {
+    if (locations.isEmpty) {
+      return [];
+    }
+
+    final polylineCoordinates = <LatLng>[];
+
+    // ユーザーの現在位置を取得する
+    final currentPosition =
+        await ref.watch(currentGeolocatorPositionProvider.future);
+
+    final polylinePoints = PolylinePoints();
+
+    for (var i = 0; i < locations.length - 1; i++) {
+      late PointLatLng pointFrom;
+      late PointLatLng pointTo;
+      if (i == 0 && currentPosition != null) {
+        // 自分の位置〜1番最初のスポットまでの経路を求める
+        pointFrom = PointLatLng(
+          currentPosition.latitude,
+          currentPosition.longitude,
+        );
+        pointTo = PointLatLng(
+          locations[i].latitude,
+          locations[i].longitude,
+        );
+      } else {
+        // i番目〜その次のスポットまでの経路を求める
+        pointFrom = PointLatLng(
+          locations[i].latitude,
+          locations[i].longitude,
+        );
+        pointTo = PointLatLng(
+          locations[i + 1].latitude,
+          locations[i + 1].longitude,
+        );
+      }
+
+      // Directions APIを用いて２点間の最適な経路を求める
+      final result = await polylinePoints.getRouteBetweenCoordinates(
+        ref.read(appInfoProvider).googleMapAPIKey,
+        pointFrom,
+        pointTo,
+        travelMode: TravelMode.walking,
+        avoidHighways: true,
+        avoidTolls: true,
+      );
+
+      if (result.points.isNotEmpty) {
+        for (final point in result.points) {
+          polylineCoordinates.add(
+            LatLng(point.latitude, point.longitude),
+          );
+        }
+      }
+    }
+    return polylineCoordinates;
+  },
+);
--- lib/presentation/page/stamp_rally/component/stamp_rally_map_view.dart
+++ lib/presentation/page/stamp_rally/component/stamp_rally_map_view.dart
@@ -2,16 +2,16 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import '../../../../application/geolocator/state/current_geolocator_position.dart';
+import '../../../../application/geolocator/state/current_poly_points.dart';
 import '../../../../application/stamp_rally/state/pin_icon.dart';
-import '../../../../domain/entity/app_info.dart';
+import '../../../../domain/entity/value_object/geo_location.dart';
 import '../../../../domain/repository/stamp_rally/entity/spot.dart';
 import '../../../../domain/repository/stamp_rally/entity/stamp_rally.dart';
+import '../../../../util/assets/assets.gen.dart';
+import '../../../component/async_value_handler.dart';
 import 'map_spot.dart';
 
 /// スタンプラリー詳細のマップ表示
@@ -30,12 +30,15 @@ class StampRallyMapView extends ConsumerStatefulWidget {
 class _StampRallyMapViewState extends ConsumerState<StampRallyMapView> {
   final Completer<GoogleMapController> mapController = Completer();
 
+  /// スポットリスト
+  List<Spot> get spots => widget.stampRally.spots;
+
+  /// スポットの座標リスト
+  List<GeoLocation> get locations =>
+      spots.map((spot) => spot.location).toList();
+
   @override
   Widget build(BuildContext context) {
-    final spots = widget.stampRally.spots;
-    final asyncValue = ref.watch(currentGeolocatorPositionProvider);
-    final icon = ref.watch(pinIconProvider);
-
     // 選択中スポットインデックスを監視してマップ上のカメラを移動する
     ref.listen(currentMapSpotIndexProvider, (previous, next) async {
       final spot = spots[next];
@@ -52,10 +55,18 @@ class _StampRallyMapViewState extends ConsumerState<StampRallyMapView> {
     return Scaffold(
       body: Stack(
         children: [
-          asyncValue.maybeWhen(
-            data: (currentPosition) =>
-                buildMapView(spots, icon, currentPosition),
-            orElse: () => buildMapView(spots, icon, null),
+          AsyncValueHandler(
+            value: ref.watch(currentPolylinePointsProvider(locations)),
+            builder: (polylinePoints) => _MapView(
+              spots: spots,
+              mapController: mapController,
+              polylinePoints: polylinePoints,
+            ),
+            loading: () => _MapView(
+              spots: spots,
+              mapController: mapController,
+              polylinePoints: const [],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(bottom: 16),
@@ -70,27 +81,54 @@ class _StampRallyMapViewState extends ConsumerState<StampRallyMapView> {
       ),
     );
   }
+}
 
-  Widget buildMapView(
-    List<Spot> spots,
-    BitmapDescriptor icon,
-    Position? currentPosition,
-  ) {
-    createPolyPoints(spots, currentPosition);
+class _MapView extends ConsumerWidget {
+  const _MapView({
+    required this.spots,
+    required this.mapController,
+    required this.polylinePoints,
+  });
+
+  final List<Spot> spots;
+  final Completer<GoogleMapController> mapController;
+  final List<LatLng> polylinePoints;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final icon = ref.watch(pinIconProvider);
     return GoogleMap(
       polylines: {
         Polyline(
           polylineId: const PolylineId('route'),
-          points: polylineCoordinates,
+          points: polylinePoints,
           width: 4,
           color: Theme.of(context).colorScheme.primaryContainer,
         )
       },
       myLocationEnabled: true,
       zoomControlsEnabled: false,
-      onMapCreated: onMapCreated,
+      onMapCreated: (controller) async {
+        mapController.complete(controller);
+        final value = await rootBundle.loadString(Assets.json.mapStyle);
+        final futureController = await mapController.future;
+        await futureController.setMapStyle(value); // Controllerを使ってMapをSetする
+      },
       myLocationButtonEnabled: false,
-      markers: createMarkersFromSpots(spots, icon),
+      // マップに表示するマーカーを生成する
+      markers: spots
+          .map(
+            (spot) => Marker(
+              infoWindow: InfoWindow(title: spot.title),
+              markerId: MarkerId(spot.id),
+              icon: icon,
+              position: LatLng(
+                spot.location.latitude,
+                spot.location.longitude,
+              ),
+            ),
+          )
+          .toSet(),
       initialCameraPosition: CameraPosition(
         target: LatLng(
           spots.first.location.latitude,
@@ -99,89 +137,5 @@ class _StampRallyMapViewState extends ConsumerState<StampRallyMapView> {
         zoom: 14,
       ),
     );
-  }
-
-  /// マップの初期構築
-  Future<void> onMapCreated(GoogleMapController controller) async {
-    mapController.complete(controller);
-    final value = await rootBundle.loadString('assets/json/map_style.json');
-    final futureController = await mapController.future;
-    await futureController.setMapStyle(value); // Controllerを使ってMapをSetする
-  }
-
-  List<LatLng> polylineCoordinates = [];
-
-  /// 経路表示用のLineを生成する
-  Future<void> createPolyPoints(
-    List<Spot> spots,
-    Position? currentPosition,
-  ) async {
-    if (spots.isEmpty || polylineCoordinates.isNotEmpty) {
-      return;
-    }
-
-    final polylinePoints = PolylinePoints();
-
-    for (var i = 0; i < spots.length - 1; i++) {
-      late PointLatLng pointFrom;
-      late PointLatLng pointTo;
-      if (i == 0 && currentPosition != null) {
-        // 自分の位置〜1番最初のスポットまでの経路を求める
-        pointFrom = PointLatLng(
-          currentPosition.latitude,
-          currentPosition.longitude,
-        );
-        pointTo = PointLatLng(
-          spots[i].location.latitude,
-          spots[i].location.longitude,
-        );
-      } else {
-        // i番目〜その次のスポットまでの経路を求める
-        pointFrom = PointLatLng(
-          spots[i].location.latitude,
-          spots[i].location.longitude,
-        );
-        pointTo = PointLatLng(
-          spots[i + 1].location.latitude,
-          spots[i + 1].location.longitude,
-        );
-      }
-
-      // Directions APIを用いて２点間の最適な経路を求める
-      final result = await polylinePoints.getRouteBetweenCoordinates(
-        ref.read(appInfoProvider).googleMapAPIKey,
-        pointFrom,
-        pointTo,
-        travelMode: TravelMode.walking,
-        avoidHighways: true,
-        avoidTolls: true,
-      );
-
-      if (result.points.isNotEmpty) {
-        for (final point in result.points) {
-          polylineCoordinates.add(
-            LatLng(point.latitude, point.longitude),
-          );
-        }
-      }
-    }
-
-    setState(() {});
-  }
-
-  /// 参加中スタンプラリーのスポットからマップに表示するマーカーを生成する
-  Set<Marker> createMarkersFromSpots(List<Spot> spots, BitmapDescriptor icon) {
-    final markers = <Marker>{};
-    for (final spot in spots) {
-      markers.add(
-        Marker(
-          infoWindow: InfoWindow(title: spot.title),
-          markerId: MarkerId(spot.id),
-          icon: icon,
-          position: LatLng(spot.location.latitude, spot.location.longitude),
-        ),
-      );
-    }
-    return markers;
   }
 }
