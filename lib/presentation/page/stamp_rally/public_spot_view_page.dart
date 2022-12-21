bef47da3d09ee8b304ea72c5b741532da68a34a6
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-12-21 15:01:51 +0900

    ♻️ refactor: 公開中スポットの一部を参加中スポットでも使用できるようにSpotViewとして別クラスに移行
    #92

--- lib/presentation/page/stamp_rally/public_spot_view_page.dart
+++ lib/presentation/page/stamp_rally/public_spot_view_page.dart
@@ -1,12 +1,10 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../application/stamp_rally/state/current_public_spot.dart';
-import '../../../application/url_launcher/url_launcher_service.dart';
 import '../../component/app_bar_title.dart';
 import '../../component/async_value_handler.dart';
-import '../../component/cached_manager.dart';
+import 'component/spot_view.dart';
 
 /// スポット詳細画面
 class PublicSpotViewPage extends StatelessWidget {
@@ -28,173 +26,10 @@ class _Body extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final size = MediaQuery.of(context).size;
-
     return AsyncValueHandler(
       value: ref.watch(currentPublicSpotProvider),
       builder: (spot) {
-        return Stack(
-          children: [
-            // スポット画像
-            CachedNetworkImage(
-              imageUrl: spot.imageUrl,
-              cacheManager: ref.watch(defaultCacheManagerProvider),
-            ),
-
-            // スポット詳細
-            Container(
-              width: size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25),
-              ),
-              margin: const EdgeInsets.only(top: 245),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 20, right: 120, top: 10),
-                    child: Text(
-                      spot.title,
-                      style: Theme.of(context).textTheme.headline6,
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.grey[350],
-                    thickness: 2,
-                  ),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.summarize,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primaryContainer,
-                                    ),
-                                    const SizedBox(
-                                      width: 7,
-                                    ),
-                                    Text(
-                                      '概要',
-                                      style: TextStyle(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .primaryContainer,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 7),
-                                Text(spot.summary)
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20, top: 7),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.location_on,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primaryContainer,
-                                    ),
-                                    const SizedBox(
-                                      width: 7,
-                                    ),
-                                    Text(
-                                      '住所',
-                                      style: TextStyle(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .primaryContainer,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      width: 7,
-                                    ),
-                                    SizedBox(
-                                      height: 27.5,
-                                      child: ElevatedButton(
-                                        onPressed: () {
-                                          final googleMapUrl = ref.read(
-                                            googleMapUrlProvider(
-                                              spot.location,
-                                            ),
-                                          );
-                                          ref
-                                              .read(urlLauncherServiceProvider)
-                                              .launch(googleMapUrl);
-                                        },
-                                        child: const Text('MAPアプリを開く'),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 7),
-                                Text(spot.address ?? '不明')
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20, top: 7),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.phone,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primaryContainer,
-                                    ),
-                                    const SizedBox(
-                                      width: 7,
-                                    ),
-                                    Text(
-                                      '電話番号',
-                                      style: TextStyle(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .primaryContainer,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                const SizedBox(height: 7),
-                                Text(
-                                  spot.tel ?? '不明',
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        );
+        return SpotView(spot: spot);
       },
     );
   }
